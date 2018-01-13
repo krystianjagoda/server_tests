a7a8005c07efbd1c1a4fa4572ddfaf93fc1b7cac
krystianjagoda <krystianjagoda@gmail.com>
2018-01-13 16:55:09 +0100

    Js Update

--- js/js_example.js
+++ js/js_example.js
@@ -1,9 +1,36 @@
-  // Create a JavaScript Date object for the current date and time, and set the desired options.
-  var date = new Date(),
-      options = { weekday: "long", month: "long", day: "numeric", year: "numeric" };
- 
-  // Convert to locale string and add a locale and the options
-  date = date.toLocaleString( "en-US", options );
- 
-  // Output the date to the above HTML element
-  document.getElementById("msg").innerHTML = date;
+// Set up!
+var a_canvas = document.getElementById("MyCanvas");
+var context = a_canvas.getContext("2d");
+
+// Draw the face
+context.fillStyle = "yellow";
+context.beginPath();
+context.arc(95, 85, 40, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+context.lineWidth = 2;
+context.stroke();
+context.fillStyle = "black";
+
+// Draw the left eye
+context.beginPath();
+context.arc(75, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the right eye
+context.beginPath();
+context.arc(114, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the mouth
+context.beginPath();
+context.arc(95, 90, 26, Math.PI, 2*Math.PI, true);
+context.closePath();
+context.fill();
+
+// Write "Hello, World!"
+context.font = "30px Garamond";
+context.fillText("Hello, World!",15,175);
+
